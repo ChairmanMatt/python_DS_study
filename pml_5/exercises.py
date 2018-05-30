a704dcd9a373c6580b5ddbc715760ee159161f9d
Matt Lu <mlu@vonage.net>
2018-05-30 15:26:27 -0400

    done PML5, will do text data next

--- pml_5/exercises.py
+++ pml_5/exercises.py
@@ -1,48 +1,81 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-import matplotlib.gridspec as gridspec
-# function to visualize data
-def visualize(data, n_sample=25):
-    fig = plt.figure(figsize=(10, 10))
-    num = int(n_sample**0.5)
-    gs = gridspec.GridSpec(num, num)
-    fig.subplots_adjust(wspace=0.01, hspace=0.02)
-    l = len(data.iloc[1, :])
-    ll = int(l**0.5)
-    for i in range(num):
-        for j in range(num):
-            temp = np.array(data.loc[(num*i + j), :]).reshape(ll, ll)
-            ax = plt.subplot(gs[i, j])
-            ax.imshow(temp, cmap=plt.cm.gray, interpolation='nearest')
-            ax.axis('off')
+def plot_inertia(km, X, n_cluster_range):
+    inertias = []
+    for i in n_cluster_range:
+        km.set_params(n_clusters=i)
+        km.fit(X)
+        inertias.append(km.inertia_)
+    plt.plot(n_cluster_range, inertias, marker='o')
+    plt.title('Elbow method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
     plt.show()
-    
-    
-import pandas as pd
-df = pd.read_csv("./data/train.csv")
-df_data = df.drop("label", 1)
-df_label = df.label
-
-visualize(df_data)
 
 
-from sklearn.decomposition import PCA
-def visualizePCA(data, n_component=100):
-    # remain first n components
-    pca = PCA(n_components = n_component)
-    df_data = pca.fit_transform(data) 
-    # convert back to original data
-    df_data = pca.inverse_transform(df_data)
-    # convert to DataFrame and plot
-    df_data = pd.DataFrame(df_data)
-    visualize(df_data)
-    
-visualizePCA(df_data) # takes a small amount of time to run
+# pic/temp.png is a 24-bit RGB image file
+# 24-bit means that 8 bits are allocated to R, G, and B. In other words, each color can have 256 different values
+img = mpimg.imread('pic/temp.png')
+#plt.figure(figsize=(10, 10))
+#imgplot = plt.imshow(img)
+#imgplot.axes.axis('off')
+#plt.show()
 
-visualizePCA(df_data, 20)
+# 3D array
+# corresponding to red, blue, green
+# all the numbers in dimension 1 and dimension 2 ranges from 0 to 255
+img_shape = img.shape
+m1, m2, m3 = img_shape
+print(img_shape)
 
-visualizePCA(df_data, 10)
+data_rs = img.reshape(m1 * m2, m3)
+print(data_rs.shape)
+print(data_rs.dtype)
 
-# PCA is different from feature selection by "combining" features into new features
-# (and assuming they are not correlated)
+# Image data is typically float32 but KMeans will throw an exception if n_clusters > 32
+# Solution is to convert image data to float64 for KMeans, and optionally back to float32 to show
+data_rs64 = data_rs.astype(np.float64)
+
+from sklearn.cluster import KMeans
+import matplotlib.image as mpimg
+
+def KmeansCompression(data, nclus=16, **kmean_kwargs):
+    '''
+    data: data to cluster
+    nclus: number of colors
+    '''
+    cluster = KMeans(n_clusters=nclus, **kmean_kwargs)
+    cluster.fit(data)
+    centers = cluster.cluster_centers_
+    labels = cluster.labels_
+    return centers[labels]
+
+#img128 = KmeansCompression(data_rs64, nclus=128, n_jobs=1)
+#img128 = img128.reshape(m1, m2, m3)
+#plt.figure(figsize=(10, 10))
+#plt.imshow(img128)
+#plt.axis('off')
+#plt.show()
+
+#img64 = KmeansCompression(data_rs64, nclus=32, n_jobs=1)
+#img64 = img64.reshape(*img_shape)
+#plt.figure(figsize=(10, 10))
+#plt.imshow(img64)
+#plt.axis('off')
+#plt.show()
+
+# On my machine, data_rs works with 16 colors but you may have to use data_rs64
+img_16 = KmeansCompression(data_rs64, nclus=16, n_jobs=1)
+img_16 = img_16.reshape(*img_shape)
+plt.figure(figsize=(10, 10))
+plt.imshow(img_16)
+plt.axis('off')
+plt.show()
+
+#n, bins, patches = plt.hist(img_16.ravel(), bins=16, range=(0.0, 1.0), fc='k', ec='k')
+#plt.show()
+
+#kmeans = KMeans(n_jobs=1)
+#plot_inertia(kmeans, data_rs64, [16, 64, 128])
